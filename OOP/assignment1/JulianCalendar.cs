4ab3381bc8d526d33e18676cde38880b09fd2c7e
Sean Hayes <seanhayes13@hotmail.com>
2018-01-09 08:54:29 -0700

    Add files via upload

---
+++ OOP/assignment1/JulianCalendar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JulianCalendar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JulianConverter jc = new JulianConverter();
+            int y = 2018;
+            int m = 1;
+            int d = 5;
+            int j = jc.dateToJulian(m, d, y);
+            Console.WriteLine(j);
+
+            int[] newDate = jc.julianToDate(j);
+            String bcad = "AD";
+            if (newDate[2] < 0)
+            {
+                bcad = "BC";
+                newDate[2] = newDate[2] * (-1);
+            }
+            Console.WriteLine(newDate[0] + " - " + newDate[1] + " - " + newDate[2] + " " + bcad);
+        }
+    }
+}
---
+++ OOP/assignment1/JulianConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JulianCalendar
+{
+    class JulianConverter
+    {
+        public int dateToJulian(int m, int d, int y)
+        {
+            int result = (1461 * (y + 4800 + (m - 14) / 12)) / 4 +
+                    (367 * (m - 2 - 12 * ((m - 14) / 12))) / 12 -
+                    (3 * ((y + 4900 + (m - 14) / 12) / 100)) / 4 +
+                    d - 32075;
+            return result;
+        }
+
+        public int[] julianToDate(int j)
+        {
+            int[] result = new int[3];
+            int l = j + 68569;
+            int n = (4 * l) / 146097;
+            l = l - (146097 * n + 3) / 4;
+            int i = (4000 * (l + 1)) / 1461001;
+            l = l - (1461 * i) / 4 + 31;
+            j = (80 * l) / 2447;
+            result[0] = l - (2447 * j) / 80;
+            l = j / 11;
+            result[1] = j + 2 - (12 * l);
+            result[2] = 100 * (n - 49) + i + l;
+            return result;
+        }
+    }
+}
