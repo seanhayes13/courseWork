e601a07866695c9ba103f8598a915af11238fbc4
Sean Hayes <seanhayes13@hotmail.com>
2018-01-18 14:16:18 -0700

    Add files via upload

--- Enums/EnumTestMain.cpp
+++ Enums/EnumTestMain.cpp
@@ -1,16 +1,38 @@
 #include <iostream>
 #include "Months.h"
+#include "DayOfWeek.h"
 #include <string>
 
 using namespace std;
 
 string getMonth(Months);
+string getDayOfWeek(DayOfWeek);
 
 void main() {
-	for (int i = 1; i < 13; i++) {
+	/*for (int i = 1; i < 13; i++) {
 		cout << getMonth(Months(i)) << endl;
 	}
+	for (int i = 0; i < 7; i++) {
+		cout << getDayOfWeek(DayOfWeek(i)) << endl;
+	}*/
+
+	int sampleJulian = 1643282;
+
+	int test[] = { 18, 1 };
+	cout << getDayOfWeek(DayOfWeek(sampleJulian%7)) << " " << getMonth(Months(test[1])) << " " << test[0] << endl;
 	system("pause");
+}
+
+string getDayOfWeek(DayOfWeek d) {
+	switch (d) {
+	case DayOfWeek::SUNDAY: return "Sunday";
+	case DayOfWeek::MONDAY: return "Monday";
+	case DayOfWeek::TUESDAY: return "Tuesday";
+	case DayOfWeek::WEDNESDAY: return "Wednesday";
+	case DayOfWeek::THURSDAY: return "Thursday";
+	case DayOfWeek::FRIDAY: return "Friday";
+	case DayOfWeek::SATURDAY: return "Saturday";
+	}
 }
 
 string getMonth(Months m) {
--- Enums/Months.h
+++ Enums/Months.h
@@ -14,3 +14,20 @@ enum class Months {
 	NOVEMBER = 11,
 	DECEMBER = 12
 };
+
+//class Months {
+//public:
+//	static const Months JANUARY;
+//	static const Months FEBRUARY;
+//
+//private:
+//	string name;
+//	int ordinal;
+//	Months(string n, int o) {
+//		this->name = n;
+//		this->ordinal = o;
+//	}
+//};
+//
+//const Months Months::JANUARY = Months("January",1);
+//const Months Months::FEBRUARY = Months("February", 2);
